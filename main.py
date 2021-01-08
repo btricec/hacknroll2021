b1693c207dc6a9db98a8386bc6521d552b1845ab
btr1ce <beatricectx@gmail.com>
2021-01-08 11:28:09 +0800

    Added tkk for the buttons

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import font as tkfont
 from PIL import ImageTk, Image
 
@@ -38,14 +39,14 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="This is the start page", font=controller.title_font)
+        label = ttk.Label(self, text="This is the start page", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
 
         self.img = ImageTk.PhotoImage(file="characters.png")
-        img_label = tk.Label(self, image=self.img)
+        img_label = ttk.Label(self, image=self.img)
         img_label.pack()
 
-        button1 = tk.Button(self, text="Start Game",
+        button1 = ttk.Button(self, text="Start Game",
                             command=lambda: controller.show_frame("PageOne"))
 ##        button2 = tk.Button(self, text="Go to Page Two",
 ##                            command=lambda: controller.show_frame("PageTwo"))
@@ -58,21 +59,30 @@ class PageOne(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="Scenario 1 goes here ", font=controller.title_font).grid(row=0, column=0, columnspan=4)
+        stats_frame = tk.Frame(self)
+        phy_health = tk.Label(stats_frame, text="Physical Health:").pack(side="left")
+        mental_health = tk.Label(stats_frame, text="Mental Health:").pack(side="left")
+        social_life = tk.Label(stats_frame, text="Social Life:").pack(side="left")
+        acad = tk.Label(stats_frame, text="Academics:").pack(side="left")
+        money = tk.Label(stats_frame, text="Money:").pack(side="left")
+
+        stats_frame.grid(row=0, column=0, columnspan=4)
+        
+        label = tk.Label(self, text="Scenario 1 goes here ", font=controller.title_font).grid(row=1, column=0, columnspan=4)
         #label.pack(side="top", fill="x", pady=10)
 
         self.img = ImageTk.PhotoImage(file="placeholder.png")
         img_label = tk.Label(self, image=self.img)
         img_label.grid(row=1, column=0, columnspan=4)
         
-        opt1 = tk.Button(self, text="Option 1").grid(row=2, column=0, columnspan=2)
-        opt2 = tk.Button(self, text="Option 2").grid(row=2, column=2, columnspan=2)
-        opt3 = tk.Button(self, text="Option 3").grid(row=3, column=0, columnspan=2)
-        opt4 = tk.Button(self, text="Option 4").grid(row=3, column=2, columnspan=2)
-        home_button = tk.Button(self, text="Home",
-                           command=lambda: controller.show_frame("StartPage")).grid(row=4, column=1)
-        next_button = tk.Button(self, text="Next",
-                                command=lambda: controller.show_frame("PageTwo")).grid(row=4, column=2)
+        opt1 = ttk.Button(self, text="Option 1").grid(row=3, column=0, columnspan=2)
+        opt2 = ttk.Button(self, text="Option 2").grid(row=3, column=2, columnspan=2)
+        opt3 = ttk.Button(self, text="Option 3").grid(row=4, column=0, columnspan=2)
+        opt4 = ttk.Button(self, text="Option 4").grid(row=4, column=2, columnspan=2)
+        home_button = ttk.Button(self, text="Home",
+                           command=lambda: controller.show_frame("StartPage")).grid(row=5, column=1)
+        next_button = ttk.Button(self, text="Next",
+                                command=lambda: controller.show_frame("PageTwo")).grid(row=5, column=2)
 
 
 class PageTwo(tk.Frame):
@@ -82,7 +92,7 @@ class PageTwo(tk.Frame):
         self.controller = controller
         label = tk.Label(self, text="Scenario 2 goes here", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Home",
+        button = ttk.Button(self, text="Home",
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
 
